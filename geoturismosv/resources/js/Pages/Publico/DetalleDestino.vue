2c5529cad7246c7aabeea23627bd1b4f8d2f0177
Ariel Yanes <arielyanes01@gmail.com>
2026-05-17 22:14:10 -0600

    añadiendo botón de Google Maps para navegación por GPS en Destinos

--- geoturismosv/resources/js/Pages/Publico/DetalleDestino.vue
+++ geoturismosv/resources/js/Pages/Publico/DetalleDestino.vue
@@ -112,6 +112,20 @@ const eliminarFavorito = () => {
                                 {{ destino.direccion ?? 'No especificada' }}
                             </p>
                         </div>
+
+                        <div v-if="destino.latitud && destino.longitud" class="mt-6 flex justify-start">
+                            <a
+                                :href="`https://www.google.com/maps/dir/?api=1&destination=${destino.latitud},${destino.longitud}`"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                class="inline-flex items-center gap-2 rounded-xl bg-[#4285F4] px-6 py-3 font-semibold text-white shadow-md transition-transform hover:scale-105 hover:bg-[#3367D6]"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
+                                    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
+                                </svg>
+                                Cómo llegar en Google Maps
+                            </a>
+                        </div>
                     </section>
 
                     <section class="mt-6">
